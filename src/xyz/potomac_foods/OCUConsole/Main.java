3348135e4a71bb1942a8ffd636c39df54ce83097
MichaelAmayaPFG <michaela@potomac-foods.com>
2020-01-29 11:27:35 -0500

    Test Hello World

--- src/xyz/potomac_foods/OCUConsole/Main.java
+++ src/xyz/potomac_foods/OCUConsole/Main.java
@@ -3,6 +3,6 @@ package xyz.potomac_foods.OCUConsole;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World");
     }
 }
